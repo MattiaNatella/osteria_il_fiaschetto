08ec80c8826f090a24d73ee95faf9a1a355337ae
MattiaNatella <mattianatella@gmail.com>
2025-05-10 17:07:11 +0200

    feat: Update homepage layout and styles; enhance DefaultLayout structure

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,41 @@
-
-
 export const Homepage = () => {
     return (
-        <div className="homepage">
-            <h1>Welcome to the Homepage</h1>
-            <p>This is the main page of our application.</p>
-        </div>
+        <>
+            <section id="jumbotron" className=""></section>
+            <div className="container jumbotron text-center border border-2">
+                <h1>La cucina Toscana nel Parco naturale di Coltano</h1>
+                <p>All’Osteria Il Fiaschetto di Coltano ti aspetta un viaggio tra le ricette della nonna e i sapori del territorio.</p>
+
+
+                <div className="row row-cols-3 orario-dove pt-3">
+
+                    <div className="col">
+                        <div className="card border-primary mb-3" >
+                            <div className="card-header"><i className="fa-solid fa-clock" style={{ color: "#971247" }}></i>Orari</div>
+                            <div className="card-body text-primary">
+
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col align-self-center">
+                        <img className="fiasco" src="./img/fiasco-header.webp" alt="" />
+                    </div>
+
+                    <div className="col">
+                        <div className="card border-primary mb-3" >
+                            <div className="card-header"><i class="fa-solid fa-location-dot" style={{ color: "#971247" }}></i>Dove Siamo</div>
+                            <div className="card-body text-primary">
+
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
